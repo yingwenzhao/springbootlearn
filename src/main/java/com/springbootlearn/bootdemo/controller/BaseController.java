e8e2c52508f78bb4d225a793e8d9c918162f5590
15091760562@163.com <15091760562@163.com>
2020-06-26 13:20:42 +0800

    "springboot  basecontroller mapping commit!!!"

--- src/main/java/com/springbootlearn/bootdemo/controller/BaseController.java
+++ src/main/java/com/springbootlearn/bootdemo/controller/BaseController.java
@@ -1,4 +1,14 @@
 package com.springbootlearn.bootdemo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BaseController {
+
+    @RequestMapping
+    public String helloWordle(){
+        return "helloWordle";
+    }
+
 }
