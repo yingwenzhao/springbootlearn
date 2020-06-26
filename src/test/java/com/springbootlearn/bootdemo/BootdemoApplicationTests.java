755c0a9854c8c53ef94c0b45dbbd674d773e0fa8
15091760562@163.com <15091760562@163.com>
2020-06-26 12:57:37 +0800

    "springboot demo first commit!!!"

---
+++ src/test/java/com/springbootlearn/bootdemo/BootdemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.springbootlearn.bootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
